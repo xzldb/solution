980f7fdd723761e5892f640ec64b2fd3bdb418a8
星光下的逗比 <92913750+xzldb@users.noreply.github.com>
2022-07-18 11:47:19 +0800

    Add files via upload

--- Solution.java
+++ Solution.java
@@ -142,20 +142,19 @@ public class Solution{
 			}
 		}
 		return returnLost;
-		
 	}
 	public static void main(String[] args) throws Exception {
 		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
 		Lost[] lostArray = new Lost[5];
-		BookLost lost1 = new BookLost("1", "鲁滨孙漂流记",df.parse("2022-02-01"), "北京市朝阳区望京宜家", "张三", "15108989899", "12345", "鲁滨孙漂流记");
+		BookLost lost1 = new BookLost("1", "鲁滨孙漂流记",df.parse("2022-02-01"), "2301教室", "张三", "15108989899", "12345", "鲁滨孙漂流记");
 		lostArray[0]=lost1;
-		BookLost lost2 = new BookLost("2", "红楼梦",df.parse("2022-06-06"), "北京市海淀区西土城地铁站", "李四", "17667675645", "34567", "红楼梦");
+		BookLost lost2 = new BookLost("2", "红楼梦",df.parse("2022-06-06"), "风华操场出口", "李四", "17667675645", "34567", "红楼梦");
 		lostArray[1]=lost2;
-		BookLost lost3 = new BookLost("3", "校园一卡通202100003",df.parse("2022-05-25"), "北京市西城区西单大悦城5层", "王五", "18234567890", "202100003", "王五");
+		BookLost lost3 = new CardLost("3", "校园一卡通202100003",df.parse("2022-05-25"), "32舍", "王五", "18234567890", "202100003", "王五");
 		lostArray[2]=lost3;
-		BookLost lost4 = new BookLost("4", "校园一卡通202100002",df.parse("2022-04-01"), "北京市顺义区 环球影城","钱六", "13178785656", "202100002", "钱六");
+		BookLost lost4 = new CardLost("4", "校园一卡通202100002",df.parse("2022-04-01"), "太极运动场","钱六", "13178785656", "202100002", "钱六");
 		lostArray[3]=lost4;
-		BookLost lost5 = new BookLost("5", "校园一卡通202100001",df.parse("2022-03-31"), "北京市丰台区良乡大学城", "冯七", "17845390987", "202100001", "冯七");
+		BookLost lost5 = new CardLost("5", "校园一卡通202100001",df.parse("2022-03-31"), "皇家一舍", "冯七", "17845390987", "202100001", "冯七");
 		lostArray[4]=lost5;
 		Solution s = new Solution();
 		s.sortLost(lostArray);
